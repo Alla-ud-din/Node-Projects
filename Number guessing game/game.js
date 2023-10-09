4422ae538da77bd6325b566914d19ed01e220d5c
Alla-ud-din <allauddin.aliahmad9@gmail.com>
2023-10-09 21:03:46 +0500

    adding guessing game file

---
+++ Number guessing game/game.js
@@ -0,0 +1,26 @@
+import chalk from 'chalk';
+import showBanner from 'node-banner';
+import inquirer from "inquirer";
+(async () => {
+    await showBanner('Welcome ! ', '\tThis is a Number Guessing Game');
+    let numberByComputer = Math.floor(10 * Math.random());
+    console.log(numberByComputer);
+    for (let i = 0; i < 3; i++) {
+        let yourGuess = await inquirer.prompt([{
+                name: "Guess",
+                type: "number",
+                message: "Enter Number:"
+            }
+        ]);
+        if (numberByComputer === yourGuess.Guess) {
+            showBanner('Congratulations', "\t\t\tYou Guess right", "yellow", "green");
+            break;
+        }
+        else if (i == 2) {
+            showBanner('Better Luck Next time', "", "red");
+        }
+        else {
+            console.log(chalk.red(`only ${2 - i} chances left`));
+        }
+    }
+})();
