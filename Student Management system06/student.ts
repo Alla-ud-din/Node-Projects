947bddbeefbef55d4bd3be3ebc087c8604b94c51
Alla-ud-din <allauddin.aliahmad9@gmail.com>
2023-10-13 02:24:22 +0500

    Updating file

---
+++ Student Management system06/student.ts
@@ -0,0 +1,37 @@
+import { error } from "console";
+import inquirer from "inquirer";
+// import { studentProfile } from "./main.js";
+// let students : any[]= []
+export async function student(selectedCourse: string){  
+let student = await inquirer.prompt([{
+    name: "Fullname",
+    type: "string",
+    message: "Enter your full Name",
+},{
+    name: "Gender",
+    type: "string",
+    message: "Enter your Gender",
+},
+// {
+//     name: "Student Id",
+//     type: "number",
+//     message: "Student Id is",
+// },
+{
+    name: "Phone number",
+    type: "number",
+    message: "Enter your phone number",
+},{
+    name: "selectedCourse",
+    type: "string",
+    message: "Selected Course",
+    default: selectedCourse
+},{
+    name: "student Id",
+    type: "string",
+    message: "Selected Course",
+    default: Math.floor(10000*Math.random()),
+}
+])
+return student;
+}
